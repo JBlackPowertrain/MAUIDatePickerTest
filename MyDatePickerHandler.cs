7e92d0bc77a72513a918ecedb25aa3e80879d1a1
Mister Black <44503036+JBlackPowertrain@users.noreply.github.com>
2023-12-22 15:17:39 -0500

    Test

---
+++ MyDatePickerHandler.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Microsoft.Maui.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDatePicker.Platforms.Android
+{
+    public class MyDatePickerHandler : DatePickerHandler
+    {
+        protected override DatePickerDialog CreateDatePickerDialog(int year, int month, int day)
+        {
+            var dialog = new DatePickerDialog(Context!, global::Android.Resource.Style.ThemeHoloLightDialog, (o, e) =>
+            {
+                if (VirtualView != null)
+                {
+                    VirtualView.Date = e.Date;
+                }
+            }, year, month, day);
+            return dialog;
+        }
+    }
+}
